0dce8dbbd8bdf1a18cf773dc1da6fb2d7c6ef89a
ACHii <achintha.tallentor@gmail.com>
2022-05-13 12:18:35 +0530

    payment page

--- resources/views/frontend/payment.blade.php
+++ resources/views/frontend/payment.blade.php
@@ -47,15 +47,31 @@
                                         <ul class="splide__list">
                                             <li class="splide__slide">
                                                 <div class="package">
+                                                    <input type="radio" class="form-check-input" name="package" value="5000">
                                                     <div class="header">
-                                                        <div class="indicator"></div>
                                                         <div class="amount"><span>Rs.</span> 5000</div>
                                                     </div>
                                                     <div class="body">Donate</div>
                                                 </div>
                                             </li>
-                                            <li class="splide__slide">Slide 02</li>
-                                            <li class="splide__slide">Slide 03</li>
+                                            <li class="splide__slide">
+                                                <div class="package">
+                                                    <input type="radio" class="form-check-input" name="package" value="10000">
+                                                    <div class="header">
+                                                        <div class="amount"><span>Rs.</span> 10000</div>
+                                                    </div>
+                                                    <div class="body">Donate</div>
+                                                </div>
+                                            </li>
+                                            <li class="splide__slide">
+                                                <div class="package">
+                                                    <input type="radio" class="form-check-input" name="package" value="15000">
+                                                    <div class="header">
+                                                        <div class="amount"><span>Rs.</span> 15000</div>
+                                                    </div>
+                                                    <div class="body">Donate</div>
+                                                </div>
+                                            </li>
                                         </ul>
                                     </div>
                                 </div>
@@ -73,16 +89,20 @@
                         <div class="profile-block">
                             <div class="title">Agent Profile</div>
                             <div class="profile-info">
-                                <img src="" alt="" class="dp">
-                                <ul class="thead">
-                                    <li>Name :</li>
-                                    <li>Area :</li>
-                                    <li>ID :</li>
-                                </ul>
-                                <ul class="tbody">
-                                    <li>Kamal</li>
-                                    <li>Colombo</li>
-                                    <li>45784154545</li>
+                                <img src="{{url('images/landing-page/nav/profile.png')}}" alt="" class="dp">
+                                <ul>
+                                    <li>
+                                        <span class="th">Name :</span>
+                                        <span class="td">Kamal</span>
+                                    </li>
+                                    <li>
+                                        <span class="th">Area :</span>
+                                        <span class="td">Colombo</span>
+                                    </li>
+                                    <li>
+                                        <span class="th">ID :</span>
+                                        <span class="td">45784154545</span>
+                                    </li>
                                 </ul>
                             </div>
                             <a href="#">View  History of this Agent</a>
@@ -90,27 +110,31 @@
                         <div class="profile-block">
                             <div class="title">Receiver's Profile</div>
                             <div class="profile-info">
-                                <img src="" alt="" class="dp">
-                                <ul class="thead">
-                                    <li>Name :</li>
-                                    <li>Area :</li>
-                                    <li>ID :</li>
-                                </ul>
-                                <ul class="tbody">
-                                    <li>Kamal</li>
-                                    <li>Colombo</li>
-                                    <li>45784154545</li>
+                                <img src="{{url('images/landing-page/nav/profile.png')}}" alt="" class="dp">
+                                <ul>
+                                    <li>
+                                        <span class="th">Name :</span>
+                                        <span class="td">Kamal</span>
+                                    </li>
+                                    <li>
+                                        <span class="th">Area :</span>
+                                        <span class="td">Colombo</span>
+                                    </li>
+                                    <li>
+                                        <span class="th">ID :</span>
+                                        <span class="td">45784154545</span>
+                                    </li>
                                 </ul>
                             </div>
                             <a href="#">View  History of this Receiver</a>
-                            <div class="cat-icon">S</div>
+                            <div class="cat-icon blue">S</div>
                         </div>
                         <div class="footer">
                             <div class="text-block">
                                 <div class="text">Your Donate amount is</div>
                                 <div class="amount"><span>Rs.</span> 5000</div>
                             </div>
-                            <img src="" alt="">
+                            <img src="{{url('images/landing-page/payment/payment.svg')}}" alt="">
                         </div>
                     </div>
                 </div>
@@ -127,9 +151,9 @@
     // Hero Slider
     var packageSlider = new Splide('#packageSlider', {
         type: 'loop',
-        perPage: 3,
         pagination: false,
         arrows: false,
+        autoWidth: true,
     });
 
     packageSlider.mount();
