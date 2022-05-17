bda599f46f3640ae6cb35a26287dc866f493fbdc
ACHii <achintha.tallentor@gmail.com>
2022-05-17 09:18:43 +0530

    receiver dashboard

--- resources/views/frontend/user/receiver.blade.php
+++ resources/views/frontend/user/receiver.blade.php
@@ -74,12 +74,13 @@
                         <div class="title">Edit this receiver profile</div>
                     </div>
                 </div>
+                <!-- Name -->
                 <div class="row g-0">
-                    <div class="col">
+                    <div class="col-md-11">
                         <label class="pro-label">Name <span>(Optional)</span></label>
                     </div>
                 </div>
-                <div class="row g-0">
+                <div class="row g-0 mb-3">
                     <div class="col-md-10">
                         <input type="text" class="form-control">
                     </div>
@@ -93,18 +94,21 @@
                             <i class="bi bi-question-circle"></i>
                             <div class="pro-tooltip">
                                 <div class="header">About toggle</div>
-                                <div class="body">You can choose whether your name display everyone or not. If you want to hide your name and profile picture you must tick this toggle. After you tick this toggle your profile picture and name will hide from your listing.</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you want
+                                    to hide your name and profile picture you must tick this toggle. After you tick this
+                                    toggle your profile picture and name will hide from your listing.</div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <!-- Nick Name -->
                 <div class="row g-0">
-                    <div class="col">
+                    <div class="col-md-11">
                         <label class="pro-label">Nick Name</label>
                     </div>
                 </div>
-                <div class="row g-0">                    
-                    <div class="col-md-11">
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">                        
                         <input type="text" class="form-control">
                     </div>
                     <div class="col-md-1">
@@ -112,32 +116,38 @@
                             <i class="bi bi-question-circle"></i>
                             <div class="pro-tooltip">
                                 <div class="header">About toggle</div>
-                                <div class="body">You can choose whether your name display everyone or not. If you want to hide your name and profile picture you must tick this toggle. After you tick this toggle your profile picture and name will hide from your listing.</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you want
+                                    to hide your name and profile picture you must tick this toggle. After you tick this
+                                    toggle your profile picture and name will hide from your listing.</div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="row g-3">
+                <!-- Age, Gender, City -->
+                <div class="row g-3 mb-3">
+                    <!-- Age -->
                     <div class="col-md-3">
                         <label class="pro-label">Age</label>
-                        <select class="form-select" >
+                        <select class="form-select">
                             <option selected disabled>Choose...</option>
                             <option>18</option>
                             <option>19</option>
                             <option>20</option>
                         </select>
                     </div>
+                    <!-- Gender -->
                     <div class="col-md-3">
                         <label class="pro-label">Gender</label>
-                        <select class="form-select" >
+                        <select class="form-select">
                             <option selected disabled>Choose...</option>
                             <option>Male</option>
                             <option>Female</option>
                         </select>
                     </div>
+                    <!-- City -->
                     <div class="col-md-5">
                         <label class="pro-label">City</label>
-                        <select class="form-select" >
+                        <select class="form-select">
                             <option selected disabled>Choose...</option>
                             <option>Maharagama</option>
                             <option>Nugegoda</option>
@@ -145,28 +155,40 @@
                         </select>
                     </div>
                 </div>
-                <div class="row g-0">
+                <!-- NIC -->
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">
+                        <label class="pro-label">NIC</label>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <!-- Address -->
+                <div class="row g-0 mb-3">
                     <div class="col-md-11">
                         <label class="pro-label">Address</label>
                         <input type="text" class="form-control">
                     </div>
                 </div>
-                <div class="row g-0">
+                <!-- Phone, Job -->
+                <div class="row g-0 mb-3">
+                    <!-- Phone -->
                     <div class="col-md-5">
                         <label class="pro-label">Phone Number</label>
                         <input type="text" class="form-control">
                     </div>
+                    <!-- Job -->
                     <div class="col-md-6">
                         <label class="pro-label">Job</label>
                         <input type="text" class="form-control">
                     </div>
                 </div>
+                <!-- Bio -->
                 <div class="row g-0">
-                    <div class="col">
+                    <div class="col-md-11">
                         <label class="pro-label">Bio</label>
                     </div>
                 </div>
-                <div class="row g-0">
+                <div class="row g-0 mb-3">
                     <div class="col-md-11">
                         <textarea class="form-control" rows="3"></textarea>
                     </div>
@@ -175,9 +197,175 @@
                             <i class="bi bi-question-circle"></i>
                             <div class="pro-tooltip">
                                 <div class="header">About toggle</div>
-                                <div class="body">You can choose whether your name display everyone or not. If you want to hide your name and profile picture you must tick this toggle. After you tick this toggle your profile picture and name will hide from your listing.</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you want
+                                    to hide your name and profile picture you must tick this toggle. After you tick this
+                                    toggle your profile picture and name will hide from your listing.</div>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <!-- Add Images -->
+                <div class="row g-0">
+                    <div class="col-md-6">
+                        <label class="pro-label">Add Images <span>(Optional)</span></label>
+                    </div>
+                    <div class="col-md-5 text-md-end">
+                        <label class="pro-label">Add 3 or more </label>
+                    </div>
+                </div>
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">
+                        <div class="media-block">
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="tooltip-block">
+                            <i class="bi bi-question-circle"></i>
+                            <div class="pro-tooltip">
+                                <div class="header">About toggle</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you want
+                                    to hide your name and profile picture you must tick this toggle. After you tick this
+                                    toggle your profile picture and name will hide from your listing.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Add videos -->
+                <div class="row g-0">
+                    <div class="col-md-6">
+                        <label class="pro-label">Add short video clips<span>(Optional)</span></label>
+                    </div>
+                    <div class="col-md-5 text-md-end">
+                        <label class="pro-label">Add 3 or more </label>
+                    </div>
+                </div>
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">
+                        <div class="media-block">
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="tooltip-block">
+                            <i class="bi bi-question-circle"></i>
+                            <div class="pro-tooltip">
+                                <div class="header">About toggle</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you want
+                                    to hide your name and profile picture you must tick this toggle. After you tick this
+                                    toggle your profile picture and name will hide from your listing.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Add audios -->
+                <div class="row g-0">
+                    <div class="col-md-11">
+                        <label class="pro-label">Add Voice cut<span>(Optional)</span></label>
+                    </div>
+                </div>
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">
+                        <div class="media-block">
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                            <a href="#"><img src="{{url('images/dashboard/placeholder.png')}}" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="tooltip-block">
+                            <i class="bi bi-question-circle"></i>
+                            <div class="pro-tooltip">
+                                <div class="header">About toggle</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you want
+                                    to hide your name and profile picture you must tick this toggle. After you tick this
+                                    toggle your profile picture and name will hide from your listing.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Requirement -->
+                <div class="row g-0">
+                    <div class="col-md-11">
+                        <label class="pro-label">Requirement</label>
+                    </div>
+                </div>
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected disabled>Choose...</option>
+                            <option>One</option>
+                            <option>Two</option>
+                            <option>Three</option>
+                        </select>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="tooltip-block">
+                            <i class="bi bi-question-circle"></i>
+                            <div class="pro-tooltip">
+                                <div class="header">About toggle</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you want
+                                    to hide your name and profile picture you must tick this toggle. After you tick this
+                                    toggle your profile picture and name will hide from your listing.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- About the donation -->
+                <div class="row g-0">
+                    <div class="col-md-11">
+                        <label class="pro-label">About the donation</label>
+                    </div>
+                </div>
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">
+                        <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                    <div class="col-md-1">
+                        <div class="tooltip-block">
+                            <i class="bi bi-question-circle"></i>
+                            <div class="pro-tooltip">
+                                <div class="header">About toggle</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you
+                                    want to hide your name and profile picture you must tick this toggle. After you
+                                    tick this toggle your profile picture and name will hide from your listing.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Account Number -->
+                <div class="row g-0">
+                    <div class="col-md-11">
+                        <label class="pro-label">Account Number</label>
+                    </div>
+                </div>
+                <div class="row g-0 mb-3">
+                    <div class="col-md-11">
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="col-md-1">
+                        <div class="tooltip-block">
+                            <i class="bi bi-question-circle"></i>
+                            <div class="pro-tooltip">
+                                <div class="header">About toggle</div>
+                                <div class="body">You can choose whether your name display everyone or not. If you
+                                    want to hide your name and profile picture you must tick this toggle. After you
+                                    tick this toggle your profile picture and name will hide from your listing.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-md-11">
+                        <a href="#" class="cta-btn btn-fill">
+                            <div class="btn-text">Save</div>
+                        </a>
                     </div>
                 </div>
             </div>
