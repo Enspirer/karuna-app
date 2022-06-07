c5480e86c9b927fbd415d8096321e7e232666341
ACHii <achintha.tallentor@gmail.com>
2022-06-07 12:29:37 +0530

    dashboard no data found

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -144,6 +144,13 @@
 
 @if(App\Models\Auth\User::where('id',auth()->user()->id)->first()->user_type == 'Agent')
 <div class="table-container">
+    @if(count(App\Models\Receivers::where('assigned_agent',auth()->user()->id)->get()) == 0)
+        @include('frontend.includes.not_found',[
+            'not_found_title' => 'No any receivers found',
+            'not_found_description' => null,
+            'not_found_button_caption' => null
+        ])
+    @else
     <table class="db-table receiver-table">
         <thead>
             <tr class="db-tr">
@@ -156,54 +163,43 @@
             </tr>
         </thead>
         <tbody>
-
-        @if(count(App\Models\Receivers::where('assigned_agent',auth()->user()->id)->get()) == 0)
-			@include('frontend.includes.not_found',[
-				'not_found_title' => 'No any receivers found',
-				'not_found_description' => null,
-				'not_found_button_caption' => null
-			])
-        @else
-            @foreach(App\Models\Receivers::where('assigned_agent',auth()->user()->id)->orderBy('id','desc')->get() as $key => $receiver)
-                <tr class="db-tr clickable-tr" data-href="{{route('frontend.user.dashboard.receiver',$receiver->id)}}">
-                    <td class="db-td">
-                        <img src="{{url('images/landing-page/nav/profile.png')}}" alt="" class="db-timg">
-                    </td>
-                    <td class="db-td">
-                        @if($receiver->name_toggle == 'yes')
-                            <div class="text">{{$receiver->nick_name}}</div>
+    @foreach(App\Models\Receivers::where('assigned_agent',auth()->user()->id)->orderBy('id','desc')->get() as $key => $receiver)
+            <tr class="db-tr clickable-tr" data-href="{{route('frontend.user.dashboard.receiver',$receiver->id)}}">
+                <td class="db-td">
+                    <img src="{{url('images/landing-page/nav/profile.png')}}" alt="" class="db-timg">
+                </td>
+                <td class="db-td">
+                    @if($receiver->name_toggle == 'yes')
+                        <div class="text">{{$receiver->nick_name}}</div>
+                    @else
+                        <div class="text">{{$receiver->name}}</div>
+                    @endif
+                </td>
+                <td class="db-td">{{$receiver->age}}</td>
+                <td class="db-td">
+                    <div class="text">{{$receiver->address}}</div>
+                </td>
+                <td class="db-td">
+                    @if(App\Models\Packages::where('id',$receiver->requirement)->first() != null)
+                        {{App\Models\Packages::where('id',$receiver->requirement)->first()->name}}
+                    @else
+                        Other
+                    @endif
+                </td>
+                <td class="db-td">
+                    <div class="status-block">
+                        @if($receiver->status == 'Task Success')
+                            <div class="indicator green"></div>
+                            <div class="status">{{$receiver->status}}</div>
                         @else
-                            <div class="text">{{$receiver->name}}</div>
+                            <div class="indicator orange"></div>
+                            <div class="status">{{$receiver->status}}</div>
                         @endif
-                    </td>
-                    <td class="db-td">{{$receiver->age}}</td>
-                    <td class="db-td">
-                        <div class="text">{{$receiver->address}}</div>
-                    </td>
-                    <td class="db-td">
-                        @if(App\Models\Packages::where('id',$receiver->requirement)->first() != null)
-                            {{App\Models\Packages::where('id',$receiver->requirement)->first()->name}}
-                        @else
-                            Other
-                        @endif
-                    </td>
-                    <td class="db-td">
-                        <div class="status-block">
-                            @if($receiver->status == 'Task Success')
-                                <div class="indicator green"></div>
-                                <div class="status">{{$receiver->status}}</div>
-                            @else
-                                <div class="indicator orange"></div>
-                                <div class="status">{{$receiver->status}}</div>
-                            @endif
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-		@endif
-
-
-
+                    </div>
+                </td>
+            </tr>
+    @endforeach
+    @endif
         </tbody>
     </table>
 </div>
@@ -211,80 +207,77 @@
 
 
 @if(App\Models\Auth\User::where('id',auth()->user()->id)->first()->user_type == 'Donor')
-
-    @if(count(App\Models\Receivers::orderBy('id','desc')->where('donor_id',auth()->user()->id)->get()) == 0)
-        @include('frontend.includes.not_found',[
-            'not_found_title' => 'No any receivers found',
-            'not_found_description' => null,
-            'not_found_button_caption' => null
-        ])
-    @else
-        <div class="table-container">
-            <table class="db-table doner-table">
-                <thead>
-                    <tr class="db-tr">
-                        <th class="db-th">Agent Name</th>
-                        <th class="db-th">Receiver Name</th>
-                        <th class="db-th">Date</th>
-                        <th class="db-th">Package</th>
-                        <th class="db-th">Donation Status</th>
-                        <th class="db-th">Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach(App\Models\Receivers::orderBy('id','desc')->where('donor_id',auth()->user()->id)->get() as $key => $receiver)
-                        <tr class="db-tr">
-                            <td class="db-td">
-                                @if(App\Models\Auth\User::where('id',$receiver->assigned_agent)->first() != null)
-                                    <div class="text">{{App\Models\Auth\User::where('id',$receiver->assigned_agent)->first()->first_name}}
-                                        {{App\Models\Auth\User::where('id',$receiver->assigned_agent)->first()->last_name}}</div>
-                                @else
-                                    <div class="text">Agent Not Found</div>
-                                @endif
-                            </td>
-                            <td class="db-td">
-                                @if($receiver->name_toggle == 'yes')
-                                    <div class="text">{{$receiver->nick_name}}</div>
-                                @else
-                                    <div class="text">{{$receiver->name}}</div>
-                                @endif
-                            </td>
-                            <td class="db-td">{{$receiver->created_at}}</td>
-                            <td class="db-td">
-                                @if(App\Models\Packages::where('id',$receiver->requirement)->first() != null)
-                                    <div class="">{{App\Models\Packages::where('id',$receiver->requirement)->first()->name}}</div>
-                                    <!-- <div class="package medicine"> -->
-                                @else
-                                    Other
-                                @endif
-                            </td>
-                            <td class="db-td">
-                                <div class="status-block">
-                                    <i class="bi completed bi-check-circle-fill"></i>
-                                    <div class="status">{{$receiver->payment_status}}</div>
-                                    <!-- <i class="bi pending bi-exclamation-circle-fill"></i>
-                                    <div class="status">Pending</div> -->
-                                </div>
-                            </td>
-                            <td class="db-td">
-                                <div class="status-block">
-                                    @if($receiver->status == 'Task Success')
-                                        <i class="bi completed bi-check-circle-fill"></i>
-                                        <div class="status">{{$receiver->status}}</div>
-                                    @else
-                                        <i class="bi pending bi-exclamation-circle-fill"></i>
-                                        <div class="status">{{$receiver->status}}</div>
-                                    @endif
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-    @endif
-
+<div class="table-container">
+        @if(count(App\Models\Receivers::orderBy('id','desc')->where('donor_id',auth()->user()->id)->get()) == 0)
+            @include('frontend.includes.not_found',[
+                'not_found_title' => 'No any receivers found',
+                'not_found_description' => null,
+                'not_found_button_caption' => null
+            ])
+        @else        
+    <table class="db-table doner-table">
+        <thead>
+            <tr class="db-tr">
+                <th class="db-th">Agent Name</th>
+                <th class="db-th">Receiver Name</th>
+                <th class="db-th">Date</th>
+                <th class="db-th">Package</th>
+                <th class="db-th">Donation Status</th>
+                <th class="db-th">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach(App\Models\Receivers::orderBy('id','desc')->where('donor_id',auth()->user()->id)->get() as $key => $receiver)
+            <tr class="db-tr">
+                <td class="db-td">
+                    @if(App\Models\Auth\User::where('id',$receiver->assigned_agent)->first() != null)
+                        <div class="text">{{App\Models\Auth\User::where('id',$receiver->assigned_agent)->first()->first_name}}
+                            {{App\Models\Auth\User::where('id',$receiver->assigned_agent)->first()->last_name}}</div>
+                    @else
+                        <div class="text">Agent Not Found</div>
+                    @endif
+                </td>
+                <td class="db-td">
+                    @if($receiver->name_toggle == 'yes')
+                        <div class="text">{{$receiver->nick_name}}</div>
+                    @else
+                        <div class="text">{{$receiver->name}}</div>
+                    @endif
+                </td>
+                <td class="db-td">{{$receiver->created_at}}</td>
+                <td class="db-td">
+                    @if(App\Models\Packages::where('id',$receiver->requirement)->first() != null)
+                        <div class="">{{App\Models\Packages::where('id',$receiver->requirement)->first()->name}}</div>
+                        <!-- <div class="package medicine"> -->
+                    @else
+                        Other
+                    @endif
+                </td>
+                <td class="db-td">
+                    <div class="status-block">
+                        <i class="bi completed bi-check-circle-fill"></i>
+                        <div class="status">{{$receiver->payment_status}}</div>
+                        <!-- <i class="bi pending bi-exclamation-circle-fill"></i>
+                        <div class="status">Pending</div> -->
+                    </div>
+                </td>
+                <td class="db-td">
+                    <div class="status-block">
+                        @if($receiver->status == 'Task Success')
+                            <i class="bi completed bi-check-circle-fill"></i>
+                            <div class="status">{{$receiver->status}}</div>
+                        @else
+                            <i class="bi pending bi-exclamation-circle-fill"></i>
+                            <div class="status">{{$receiver->status}}</div>
+                        @endif
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+        @endif
+        </tbody>
+    </table>
+</div>   
 @endif
 
 @if(App\Models\Auth\User::where('id',auth()->user()->id)->first()->user_type == 'Receiver')
