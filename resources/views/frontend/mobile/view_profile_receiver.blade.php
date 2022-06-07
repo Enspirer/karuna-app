d296e5980cbd09a18550907742f6c01b44c61246
ACHii <achintha.tallentor@gmail.com>
2022-06-07 19:34:11 +0530

    back btn

--- resources/views/frontend/mobile/view_profile_receiver.blade.php
+++ resources/views/frontend/mobile/view_profile_receiver.blade.php
@@ -8,7 +8,8 @@
 <section class="app-bar-section">
     <div class="mobile-container">
         <div class="inner-wrapper">
-            <a href="{{route('frontend.user.mobile.index')}}" class="back-btn">
+            <!-- <a href="{{route('frontend.user.mobile.index')}}" class="back-btn"> -->
+            <a href="#" onclick="history.back()" class="back-btn">
                 <i class="fa-solid fa-arrow-left"></i>
             </a>
             <div class="title">
