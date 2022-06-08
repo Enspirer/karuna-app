e83bee3eac7785b4d05ef70f08a74d07057ca49e
nihsaan7 <nihsaan.enspirer@gmail.com>
2022-06-08 15:09:04 +0530

    some changes

--- resources/views/frontend/mobile/receiver_agent.blade.php
+++ resources/views/frontend/mobile/receiver_agent.blade.php
@@ -28,7 +28,11 @@
             <div class="header" style="background-image: url('{{uploaded_asset($receiver->cover_image)}}');">
 
                 <div class="dp-block">
-                    <img src="{{ uploaded_asset($receiver->profile_image) }}" alt="">
+                    @if($receiver->profile_image != null)
+                        <img src="{{uploaded_asset($receiver->profile_image)}}" alt="">
+                    @else
+                        <img src="{{url('images/landing-page/nav/profile.png')}}" alt="">
+                    @endif
                 </div>
             </div>
             @if($receiver->name_toggle == 'yes')
