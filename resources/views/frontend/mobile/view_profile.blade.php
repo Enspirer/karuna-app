94029d53dc446d9e4b133271e158f469d8211211
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2022-06-08 10:01:46 +0530

    View Profile

--- resources/views/frontend/mobile/view_profile.blade.php
+++ resources/views/frontend/mobile/view_profile.blade.php
@@ -123,6 +123,9 @@
                                     <div id="char{{$receiver->id}}" class="accordion-collapse collapse" aria-labelledby="charHead{{$receiver->id}}"
                                         data-bs-parent="#charityList">
                                         <div class="accordion-body">
+                                            <div>
+                                                Receiver Name: <a href="{{route('frontend.user.mobile.view_profile_receiver')}}"> {{$receiver->name}}</a>
+                                            </div>
                                             <div class="title">Description</div>
                                             <div class="text">{{$receiver->about_donation}}</div>
                                         </div>
