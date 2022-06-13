4b639a38fa83277abee3a1df6d53f73f5c9dc81a
ACHii <achintha.tallentor@gmail.com>
2022-06-13 09:50:44 +0530

    nic modal

--- resources/views/frontend/mobile/index.blade.php
+++ resources/views/frontend/mobile/index.blade.php
@@ -305,7 +305,40 @@
 @endif
 
 
-
+<!-- NIC modal -->
+<div class="modal fade nic-modal" id="nicModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="nicModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="title">Modal Title</div>
+        <p>Please enter your NIC details to continue</p>
+      </div>
+      <div class="modal-body">
+        <div class="row g-3">
+            <div class="col-12">
+                <label for="nic_no" class="form-label">NIC Number</label>
+                <input type="text" class="form-control" name="nic_no" id="nic_no" required>
+            </div>
+            <div class="col-12">
+                <label for="nic_img" class="form-label">NIC Photo</label>
+                <div class="input-group">
+                    <input type="file" class="form-control" name="nic_img" id="nic_img" accept="image/png, image/jpeg"  multiple="multiple" required>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <div class="row">
+            <div class="col-12">
+                <button type="submit" class="cta-btn btn-fill">
+                    <div class="btn-text">Submit</div>
+                </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 @endsection
@@ -321,6 +354,15 @@
     });
 
     homeSlider.mount();
+</script>
+
+<script>
+    $(window).on('load', function () {
+        $('#nicModal').modal('show');
+    });
+    $("#close-btn").click(function () {
+        $('#nicModal').modal('hide');
+    });
 </script>
 
 
